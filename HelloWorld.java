40743bcebbc88d6f594c70a079b4822be581e357
divakarmanchala6 <divakar.manchala6@gmail.com>
2024-12-11 12:17:55 +0530

    check commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,34 +1,6 @@
-import java.util.Scanner;
-
 public class HelloWorld {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Input from user
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-
-        // Check if the number is prime
-        if (isPrime(number)) {
-            System.out.println(number + " is a prime number.");
-        } else {
-            System.out.println(number + " is not a prime number.");
-        }
-
-        scanner.close();
-    }
-
-    // Method to check if a number is prime
-    public static boolean isPrime(int num) {
-        if (num <= 1) {
-            return false; // Numbers less than or equal to 1 are not prime
-        }
-
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false; // If divisible by any number other than 1 and itself
-            }
-        }
-        return true; // Number is prime
+    public static void main(String []args) {
+        System.out.println("Hello World");
     }
 }
+ 
---
+++ PrimeNumbersCheck.java
@@ -0,0 +1,6 @@
+public class PrimeNumbersCheck {
+    public static void main(String []args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
